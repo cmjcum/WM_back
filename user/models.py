befaa1c01e26f8e1d263f2f64321b9aad08d169d
LULULALA2 <lhg9714@gmail.com>
2022-07-11 16:11:48 +0900

    CLN

--- user/models.py
+++ user/models.py
@@ -1,7 +1,3 @@
-from tkinter import CASCADE
-from unicodedata import name
-from django.db import models
-
 # Create your models here.
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
