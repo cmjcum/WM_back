947c7462ffa19dbf3a97076037832be200eedabc
LULULALA2 <lhg9714@gmail.com>
2022-07-11 14:56:57 +0900

    ENH:add myroom model

--- myroom/admin.py
+++ myroom/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.Furniture)
+admin.site.register(models.MyFurniture)
+admin.site.register(models.FurniturePosition)
