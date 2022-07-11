5a41b62079390fb362b584446c6f6ce7036fac3e
yinmsk <galaxy220111@gmail.com>
2022-07-11 16:08:33 +0900

    ENH: usermanager class 추가

--- user/models.py
+++ user/models.py
@@ -3,6 +3,27 @@ from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
 
+class UserManager(BaseUserManager):
+    def create_user(self, username, password=None):
+        if not username:
+            raise ValueError('Users must have an username')
+        user = self.model(
+            username=username,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, username, password=None):
+        user = self.create_user(
+            username=username,
+            password=password
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
 class User(AbstractBaseUser):
     username = models.CharField("사용자 이름", max_length=20, unique=True)
     nickname = models.CharField("게시판 이름", max_length=20, unique=True)
