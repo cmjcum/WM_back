632f22ae821a0e7fc6cb1d8ecd94044f44039971
m_ink <mp98dl4@gmail.com>
2022-07-12 23:48:54 +0900

    ENH models __str__ 적용

--- board/models.py
+++ board/models.py
@@ -9,6 +9,9 @@ class Article(models.Model):
     picture_url = models.URLField(blank=True)
     create_date = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f'[{self.planet.name} | {self.author.nickname}]  {self.title}'
+
 
 class Comment(models.Model):
     author = models.ForeignKey('user.User', on_delete=models.CASCADE)
@@ -16,3 +19,9 @@ class Comment(models.Model):
     content = models.TextField()
     create_date = models.DateTimeField(auto_now_add=True)
     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        if self.parent == None:
+            return f'[{self.article.title}]  {self.content}'
+
+        return f'[{self.article.title}]  {self.content} ({self.parent.id}의 대댓글)'
