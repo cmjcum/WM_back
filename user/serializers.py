4529d0d5e11db0b6a9ea1f488a7f7241dfd1b1b1
zeonga1102 <dlwjddk4587@naver.com>
2022-07-26 16:15:56 +0900

    ENH: PlanetSerializer 추가

--- user/serializers.py
+++ user/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from user.models import User as UserModel
+from .models import Planet
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -15,3 +16,26 @@ class UserSerializer(serializers.ModelSerializer):
         user.save()
 
         return user
+
+
+class PlanetSerializer(serializers.ModelSerializer):
+    empty_rooms = serializers.SerializerMethodField(read_only=True)
+
+    def get_empty_rooms(self, obj):
+        empty_rooms = []
+        for floor in range(1, obj.max_floor+1):
+            floor_rooms = []
+
+            using_rooms = [user_info.room_number for user_info in obj.userinfo_set.filter(floor=floor)]
+
+            for room_number in range(1, obj.max_number+1):
+                if room_number not in using_rooms:
+                    floor_rooms.append(room_number)
+            if floor_rooms != []:
+                empty_rooms.append({floor: floor_rooms})
+
+        return empty_rooms
+
+    class Meta:
+        model = Planet
+        fields = ['name', 'max_floor', 'max_number', 'empty_rooms']
