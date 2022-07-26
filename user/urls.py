2636a5795c82d60bf75fa956b1d02f9b08b2ab33
zeonga1102 <dlwjddk4587@naver.com>
2022-07-26 16:21:30 +0900

    ENH: PlanetView url에 연결

--- user/urls.py
+++ user/urls.py
@@ -5,10 +5,12 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from .views import PlanetView
 
 
 urlpatterns = [
     path('', UserView.as_view()),
+    path('planet/', PlanetView.as_view()),
 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
