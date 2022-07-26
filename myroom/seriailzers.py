ff65dda953cd79ffbad75a7b4d7792178ee1c2cb
yinmsk <galaxy220111@gmail.com>
2022-07-26 23:12:01 +0900

    CLN

--- myroom/seriailzers.py
+++ myroom/seriailzers.py
@@ -1,7 +1,9 @@
+from requests import Response
 from rest_framework import serializers
 
 from myroom.models import GuestBook as GuestBookModel
 from user.models import UserInfo as UserInfoModel
+from user.models import User as UserModel
 
 from django.utils import timezone, dateformat
 
@@ -11,14 +13,39 @@ from myroom.models import FurniturePosition as FurniturePositionModel
 from user.models import UserManager as UserManagerModel
 from user.models import ArticleLike as ArticleLikeModel
 from user.models import Planet as PlanetModel
-from user.models import User as UserModel
+
+
+class UserModelSerializer(serializers.ModelSerializer):
+    like_count = serializers.SerializerMethodField()
+    follow_count = serializers.SerializerMethodField()
+    following_count = serializers.SerializerMethodField()
+
+    def get_like_count(self, obj):
+        like_count = obj.like.count()
+        return like_count
+
+    def get_follow_count(self, obj):
+        follow_count = obj.follow.count()
+        return follow_count
+
+    def get_following_count(self, obj):
+        return "테스트"
+
+    class Meta:
+        model = UserModel
+        fields = ["like_count", "follow_count" ,"following_count"]
 
 
 class UserInfoModelSerializer(serializers.ModelSerializer):
+    birthday = serializers.SerializerMethodField()
+    user = UserModelSerializer()
+
+    def get_birthday(self, obj):
+        return dateformat.format(obj.birthday, 'm.d')
 
     class Meta:
         model = UserInfoModel
-        fields = ["name", "birthday", "portrait", "coin"]
+        fields = ["name", "birthday", "portrait", "coin", "user_id", "user"]
 
 
 class RoomDataSerializer(serializers.ModelSerializer):
@@ -35,6 +62,11 @@ class PostGuestBookModelSerializer(serializers.ModelSerializer):
 
 class GetGuestBookModelSerializer(serializers.ModelSerializer):
     nickname = serializers.SerializerMethodField(read_only=True)
+    create_date = serializers.SerializerMethodField()
+
+    def get_create_date(self, obj):
+        return dateformat.format(obj.create_date, 'y.m.d')
+
     def get_nickname(self, obj):
         return obj.author.nickname
 
