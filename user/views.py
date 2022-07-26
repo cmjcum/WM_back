d6e2e4d4b179e08394cd42696b56ca033f909fd5
zeonga1102 <dlwjddk4587@naver.com>
2022-07-26 11:10:26 +0900

    딥러닝 예시 주석문 추가

--- user/views.py
+++ user/views.py
@@ -4,6 +4,11 @@ from rest_framework import permissions, status
 
 from user.serializers import UserSerializer
 
+from multiprocessing import Process, Queue
+import boto3
+
+from deeplearning.deeplearning_make_portrait import make_portrait
+
 
 class UserView(APIView):
     permission_classes = [permissions.AllowAny]
@@ -16,8 +21,39 @@ class UserView(APIView):
                 
         return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
+q = Queue()
+p = None
 class UserInfoView(APIView):
     def post(self, request):
         
-        pass
+        # global q, p
+        
+        # user_id = request.user.id
+        # request.data['user'] = user_id
+        
+        # pic = request.data.pop('pic')[0]
+        # filename = pic.name
+
+        # s3 = boto3.client('s3')
+        # s3.put_object(
+        #     ACL="public-read",
+        #     Bucket="my-sparta",
+        #     Body=pic,
+        #     Key=filename,
+        #     ContentType=pic.content_type)
+
+        # url = f'https://my-sparta.s3.ap-northeast-2.amazonaws.com/{filename}'
+        # request.data['pic'] = url
+
+        # original_pic_serializer = OriginalPicSerializer(data=request.data)
+
+        # if original_pic_serializer.is_valid():
+        #     original_pic_serializer.save()
+
+        #     p = Process(target=make_portrait, args=(q, url, user_id))
+        #     p.start()
+
+        #     return Response({'msg': 'send'}, status=status.HTTP_200_OK)
+
+        return Response({"error": "failed"}, status=status.HTTP_400_BAD_REQUEST)
+
