d91b8d9e8a10d55efad907ff7bdc2cde11a0e569
zeonga1102 <dlwjddk4587@naver.com>
2022-07-28 20:41:11 +0900

    ENH: 유효성 검사를 통과했을 때만 s3에 파일 저장하고 딥페이크 수행하게 변경

--- user/views.py
+++ user/views.py
@@ -14,6 +14,8 @@ from .models import Planet
 
 from datetime import datetime
 
+from .serializers import BasicUserInfoSerializer
+
 
 class UserView(APIView):
     permission_classes = [permissions.AllowAny]
@@ -31,27 +33,32 @@ p = None
 class UserInfoView(APIView):
     def post(self, request):
         global q, p
-        
-        request.data['user'] = request.user.id
-        
-        # user_id = request.user.id
-        # request.data['user'] = user_id
-        
-        pic = request.data.pop('portrait')[0]
-        filename = datetime.now().strftime('%Y%m%d%H%M%S%f') + pic.name
-        
-        s3 = boto3.client('s3')
-        s3.put_object(
-            ACL="public-read",
-            Bucket="wm-portrait",
-            Body=pic,
-            Key=filename,
-            ContentType=pic.content_type)
 
-        url = f'https://wm-portrait.s3.ap-northeast-2.amazonaws.com/{filename}'
-        # make_portrait(q, url, request.user.id)
-        p = Process(target=make_portrait, args=(q, url, request.user.id))
-        p.start()
+        request.data['user'] = request.user.id
+        pic = request.data.pop('portrait')[0]
+
+        basic_user_info_serializer = BasicUserInfoSerializer(data=request.data)
+        if basic_user_info_serializer.is_valid():
+            q.put(request.data)
+        
+            filename = datetime.now().strftime('%Y%m%d%H%M%S%f') + pic.name
+        
+            s3 = boto3.client('s3')
+            s3.put_object(
+                ACL="public-read",
+                Bucket="wm-portrait",
+                Body=pic,
+                Key=filename,
+                ContentType=pic.content_type)
+
+            # s3에 저장 안 하고 바로 파일 자체를 읽어서 딥페이크를 적용할 수는 없을까
+            # imageio로 파일 읽는 방법?
+            url = f'https://wm-portrait.s3.ap-northeast-2.amazonaws.com/{filename}'
+            # make_portrait(q, url, request.user.id)
+            p = Process(target=make_portrait, args=(q, url, request.user.id))
+            p.start()
+
+            return Response(status=status.HTTP_200_OK)
         # request.data['pic'] = url
 
         # original_pic_serializer = OriginalPicSerializer(data=request.data)
