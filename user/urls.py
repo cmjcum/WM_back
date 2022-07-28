bcd5aef5ef9081f0f64655886ce3c47200f6f75b
zeonga1102 <dlwjddk4587@naver.com>
2022-07-28 17:10:53 +0900

    ENH: UserInfoView url 연결

--- user/urls.py
+++ user/urls.py
@@ -6,10 +6,12 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 from .views import PlanetView
+from .views import UserInfoView
 
 
 urlpatterns = [
     path('', UserView.as_view()),
+    path('user_info/', UserInfoView.as_view()),
     path('planet/', PlanetView.as_view()),
 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
