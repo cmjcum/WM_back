f996686830eb2800d008254948ee34a4fae70082
zeonga1102 <dlwjddk4587@naver.com>
2022-08-01 11:35:58 +0900

    Add permissions.py

---
+++ makemigrations/permissions.py
@@ -0,0 +1,9 @@
+from rest_framework.permissions import BasePermission
+from rest_framework.exceptions import APIException
+from rest_framework import status
+
+
+class GenericAPIException(APIException):
+    def __init__(self, status_code, detail=None, code=None):
+        self.status_code=status_code
+        super().__init__(detail=detail, code=code)
