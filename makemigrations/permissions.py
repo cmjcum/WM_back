bf94c71c104ac723eaccf14fbd44ba4219e09079
zeonga1102 <dlwjddk4587@naver.com>
2022-08-01 17:55:48 +0900

    ENH: UserInfo가 없는 사용자만 접근 가능하도록 퍼미션 커스텀

--- makemigrations/permissions.py
+++ makemigrations/permissions.py
@@ -1,9 +1,29 @@
 from rest_framework.permissions import BasePermission
 from rest_framework.exceptions import APIException
 from rest_framework import status
+from user.models import UserInfo
 
 
 class GenericAPIException(APIException):
     def __init__(self, status_code, detail=None, code=None):
         self.status_code=status_code
         super().__init__(detail=detail, code=code)
+
+
+class HasNoUserInfoUser(BasePermission):
+
+    message = '이주가 완료되지 않은 유저만 접근할 수 있는 페이지입니다.'
+
+    def has_permission(self, request, view):
+        user = request.user
+
+        if not user.is_authenticated:
+            response ={
+                    "detail": "서비스를 이용하기 위해 로그인 해주세요.",
+                }
+            raise GenericAPIException(status_code=status.HTTP_401_UNAUTHORIZED, detail=response)
+
+        if UserInfo.objects.filter(user__id=user.id):
+            return False
+        
+        return True
