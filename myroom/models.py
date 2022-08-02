465dc5cf3ed59e7265899e5c9257dac8bfced82b
zeonga1102 <dlwjddk4587@naver.com>
2022-08-02 14:37:11 +0900

    CLN: 사용하지 않는 필드 제거

--- myroom/models.py
+++ myroom/models.py
@@ -4,8 +4,6 @@ from django.db import models
 # 가구
 class Furniture(models.Model):
     name = models.CharField(max_length=15)
-    width = models.IntegerField()
-    height = models.IntegerField()
     url_left = models.URLField()
     url_right = models.URLField()
     price = models.IntegerField()
