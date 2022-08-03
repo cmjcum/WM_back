0a8bd5ea675dd4b6954b160f6518ab4070d20c0c
LULULALA2 <lhg9714@gmail.com>
2022-08-03 13:49:34 +0900

    CLN

--- user/jwt_claim_serializer.py
+++ user/jwt_claim_serializer.py
@@ -15,18 +15,13 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
         try:
             token['coin'] = user.userprofile.coin
             # print('last_login:', user.userinfo.last_date, 'today:', datetime.date.today())
-            print('coin: ', token['coin'])
+
             token['today_pt'] = bool(user.userprofile.last_date != datetime.date.today())
             if token['today_pt']:
                 user_info = UserInfo.objects.get(id=user.userprofile.id)
-                print(user.userprofile.id)
-                print(user_info)
                 user_info.coin += 100
-                print('100pt 지급!')
                 user_info.save()
                 token['coin'] = user_info.coin
-
-            print('user_id:', token['user_id'], 'username:', token['username'], 'planet:', token['planet'], 'today_pt:', token['today_pt'], 'coin: ', token['coin'])
 
         except:
             pass
