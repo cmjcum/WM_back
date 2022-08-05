fc76f44efd854ade19841abb084d32d22d00a165
LULULALA2 <lhg9714@gmail.com>
2022-08-05 18:48:55 +0900

    도메인 변경

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(makemigrations.click www.makemigrations.click)
+domains=(coumi.makemigrations.click)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="lhg9714@gmail.com" # Adding a valid address is strongly recommended
